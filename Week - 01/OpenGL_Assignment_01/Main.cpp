f280b7cb1771a424fc2830878cc4802b9e74712a
Bunleap Beun <bunleap997@gmail.com>
2025-02-16 17:11:02 +0700

    first commit

---
+++ Week - 01/OpenGL_Assignment_01/Main.cpp
@@ -0,0 +1,19 @@
+#include <GL/glut.h>
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0.0, 0.0); // Red color
+	// Draw a simple triangle
+	glBegin(GL_TRIANGLES);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(0.5, -0.5);
+	glVertex2f(0.0, 0.5);
+	glEnd();
+	glFlush();
+}
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutCreateWindow("First OpenGL Program");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
